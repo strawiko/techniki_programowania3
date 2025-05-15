3f33a8c809cd3aa8b8d074244c6f282c9f673258
strawwiko <filipstrawa@gmail.com>
2025-05-15 15:46:06 +0200

    dziala

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <matplot/matplot.h>
 #include<pybind11/pybind11.h>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int add() {
+    int a = 5;
+    int b = 4;
+    return a + b;
 }//zdwad
+PYBIND11_MODULE(example, m) {
+    m.doc() = "Example module using pybind11"; // optional module docstring
+
+    // Expose functions to Python
+    m.def("add", &add, "A function that adds two numbers");
+}
