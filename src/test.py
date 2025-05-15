1d65e4b145e835ebc17454974df8df5338b7a27d
Filip Strawa <s203655@student.pg.edu.pl>
2025-05-15 19:52:29 +0200

    asd

--- src/test.py
+++ src/test.py
@@ -1,5 +1,9 @@
 import sys
-sys.path.append('build/Debug')  # lub 'build/Debug' jeśli tam jest plik
+import os
+
+
+print(os.listdir('../build/Debug'))
+sys.path.append('../build/Debug')  # lub 'build/Debug' jeśli tam jest plik
 import example
 
 print(example.add())
