a1faaecd60aa390992706560ebaa64eb987337ca
strawwiko <filipstrawa@gmail.com>
2025-05-25 19:30:05 +0200

    --

--- src/dft.cpp
+++ src/dft.cpp
@@ -4,21 +4,26 @@
 #include <complex>
 #include "headers.h"
 
-std::vector<std::complex<double>> dft(const std::vector<double>& samples) {
+std::vector<double> dft(Signal signal) {
 
-    std::vector<std::complex<double>> X(samples.size());
-    const double scale = 1.0 / std::sqrt(samples.size());
+
+    std::vector<double> Y;
+    std::vector<std::complex<double>> X(signal.samples.size());
+    const double scale = 1.0 / std::sqrt(signal.samples.size());
     
-    for (int k = 0; k < samples.size(); k++) {
+    for (int k = 0; k < signal.samples.size(); k++) {
         X[k] = std::complex<double>(0.0, 0.0);
-        for (int n = 0; n < samples.size(); n++) {
-            double angle = 2.0 * PI * k * n / samples.size();
+        for (int n = 0; n < signal.samples.size(); n++) {
+            double angle = 2.0 * PI * k * n / signal.samples.size();
             double real = std::cos(angle);
             double imag = -std::sin(angle);
-            X[k] += samples[n] * std::complex<double>(real, imag);
+            X[k] += signal.samples [n] * std::complex<double>(real, imag);
         }
         X[k] *= scale;  // Normalization
     }
-    
-    return X;
+    Y.reserve(X.size());
+    for (int i = 0; i < X.size(); i++) {
+        Y.push_back(X[i].real());
+    }
+    return Y;
 }
